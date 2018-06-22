601f9ed297e4fb72e2abb90d35d7a5b357caf9c7
Carlos GC <mail@ccebrecos.com>
2018-06-22 13:56:45 +0200

    Travis divided into stages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-travis-test",
-    version="0.0.3",
+    version="0.0.4",
     author="davidlj95 && ccebrecos",
     author_email="ccebrecos@davidlj95.com",
     description="Tests PyPi deployments with Travis",
