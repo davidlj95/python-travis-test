cdb20258735b0ad26f7a3effb003d0886b9587ba
David LJ <mail@davidlj95.com>
2018-06-22 12:04:54 +0200

    Speed up build by separing test and style checks

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+# Runs the specificated tests
+# Specify the test to run using the $TEST_SUITE environment variable
+# If no variable $TEST_SUITE is found, or if it is empty, all tests will be
+# run
+# 
+# Available tests: "tests", "style"
 
-python -m unittest discover tests
-python -m flake8 bitcoin tests
+# Python unittests
+if [ -z "$TEST_SUITE" ] || [ "$TEST_SUITE" = "tests" ]; then
+	python -m unittest discover tests
+fi
+
+# Python style check
+if [ -z "$TEST_SUITE" ] || [ "$TEST_SUITE" = "style" ]; then
+	python -m flake8 bitcoin tests
+fi
