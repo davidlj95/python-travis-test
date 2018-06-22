4ed6806b98f9e02da46b7ddf7ea08e7b8b8e3aa0
Carlos GC <mail@ccebrecos.com>
2018-06-22 13:09:14 +0200

    New version 0.0.2

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -1,3 +1,4 @@
 from bitcoin.encoding import b58encode, b58decode, bech32encode, bech32decode
 name = "bitcoin"
+version = "0.0.2"
 __all__ = ["b58encode", "b58decode", "bech32encode", "bech32decode"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-travis-test",
-    version="0.0.1",
+    version="0.0.2",
     author="davidlj95 && ccebrecos",
     author_email="ccebrecos@davidlj95.com",
     description="Tests PyPi deployments with Travis",
