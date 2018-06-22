4f482b987fb1ede9afdcd87c09ecf43691d0a48d
David LJ <mail@davidlj95.com>
2018-06-22 12:57:23 +0200

    Python packaging for PyPi

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -1,3 +1,3 @@
 from bitcoin.encoding import b58encode, b58decode, bech32encode, bech32decode
-
+name = "bitcoin"
 __all__ = ["b58encode", "b58decode", "bech32encode", "bech32decode"]
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-travis-test",
+    version="0.0.1",
+    author="davidlj95 && ccebrecos",
+    author_email="ccebrecos@davidlj95.com",
+    description="Tests PyPi deployments with Travis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidlj95/python-travis-test",
+    packages=setuptools.find_packages(),
+    classifiers=(
+	"Programming Language :: Python :: 3.5",
+	"Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+    ),
+)
