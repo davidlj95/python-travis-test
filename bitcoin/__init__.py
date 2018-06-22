42cb3212c27006ff2109b3f2bb39d2111f442d3c
Carlos GC <mail@ccebrecos.com>
2018-06-22 14:10:14 +0200

    Fixed travis conditional staging

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -1,4 +1,4 @@
 from bitcoin.encoding import b58encode, b58decode, bech32encode, bech32decode
 name = "bitcoin"
-version = "0.0.4"
+version = "0.0.5"
 __all__ = ["b58encode", "b58decode", "bech32encode", "bech32decode"]
